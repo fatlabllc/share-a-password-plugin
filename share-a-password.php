f8574e7efc8c533fcbc140cd0b456c43e97890c2
S. Larrabee <slarrabee@fatlabllc.com>
2017-05-24 10:31:35 -0600

    updated add_option function (works now)

--- share-a-password.php
+++ share-a-password.php
@@ -19,7 +19,7 @@ License: GPL2
 function sap_setup()
 {
 	// store plugin version in wp_options table
-	add_option('shareapassword_version', '0.2');
+	add_option( 'sap_version', '0.4', '', 'no' );
 	// create our main table
     global $wpdb;
 	$table_name = $wpdb->prefix . "shareapassword";
